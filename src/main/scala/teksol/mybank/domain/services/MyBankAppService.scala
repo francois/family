2ac405d4d030e8fc7a8debfe554d6c3b3e4fe235
François Beausoleil <francois@teksol.info>
2017-03-05 08:39:48 -0500

    Fixed applyInterestsToAllFamilies() wasn't active yet

--- src/main/scala/teksol/mybank/domain/services/MyBankAppService.scala
+++ src/main/scala/teksol/mybank/domain/services/MyBankAppService.scala
@@ -1,9 +1,12 @@
 package teksol.mybank.domain.services
 
 import java.time.LocalDate
+import java.util.UUID
 
+import com.sun.org.glassfish.gmbal.Description
 import teksol.domain.FamilyId
 import teksol.infrastructure._
+import teksol.mybank.domain.events.InterestPosted
 import teksol.mybank.domain.models._
 import teksol.mybank.infrastructure.MyBankRepository
 
@@ -11,42 +14,34 @@ class MyBankAppService(private[this] val repository: MyBankRepository, private[t
     def findFamily(familyId: FamilyId): Family = repository.findFamily(familyId)
 
     def applyInterestsToAllFamilies(i18n: I18n, postedOn: LocalDate): Unit = {
-        throw new RuntimeException("TODO")
-        //        private def publishInterestEntriesCreated(entries: Set[Entry]) =
-        //        entries.map(entryToInterestPosted).foreach(eventBus.publish)
-        //
-        //        private[this] def entryToInterestPosted(entry: Entry) =
-        //        InterestPosted(entry.familyId, entry.accountId, entry.postedOn, entry.amount)
+        def publishInterestEntriesCreated(entries: Set[Entry]) =
+            entries.map(entryToInterestPosted).foreach(eventBus.publish)
 
-        /*
+        def entryToInterestPosted(entry: Entry) =
+            InterestPosted(entry.familyId, entry.accountId, entry.postedOn, entry.amount)
 
-            def applyInterestsToAllFamilies(i18n: I18n, postedOn: LocalDate): Unit = {
-                val accounts = findAccountsWithInterestRates.asScala.toSet
-                val entries = buildInterestEntries(i18n, accounts, postedOn)
-                postInterestEntriesToDatabase(postedOn, entries)
-            }
-
-            private[this] def buildInterestEntries(i18n: I18n, accounts: Set[AccountWithInterest], postedOn: LocalDate) = {
-                def buildDescription(account: AccountWithInterest) = {
-                    val formattedBalance = i18n.amountWithDelimiter(account.locale, account.balance)
-                    val formattedRate = i18n.numberToPercentage(account.locale, account.yearlyInterestRate)
-                    val i18nKey = account.balance match {
-                        case Amount.ZERO => "interests.none"
-                        case balance if balance.isNegative => "interests.negative"
-                        case _ => "interests.positive"
-                    }
-
-                    i18n.translate(account.locale, i18nKey, params = Map("rate" -> formattedRate, "balance" -> formattedBalance)).map(Description.apply).get
+        def buildEntriesForInterestBearingAccounts(accounts: Set[AccountWithInterest]) = {
+            def buildDescription(account: AccountWithInterest) = {
+                val formattedBalance = i18n.amountWithDelimiter(account.locale, account.balance)
+                val formattedRate = i18n.numberToPercentage(account.locale, account.yearlyInterestRate)
+                val i18nKey = account.balance match {
+                    case Amount.ZERO => "interests.none"
+                    case balance if balance.isNegative => "interests.negative"
+                    case _ => "interests.positive"
                 }
 
-                accounts.map { account =>
-                    Entry(account.familyId, account.accountId, EntryId(UUID.randomUUID()), postedOn, buildDescription(account), account.interests)
-                }
+                i18n.translate(account.locale, i18nKey, params = Map("rate" -> formattedRate, "balance" -> formattedBalance)).map(EntryDescription.apply).get
             }
 
-         */
+            accounts.map { account =>
+                Entry(account.familyId, account.accountId, EntryId(UUID.randomUUID()), postedOn, buildDescription(account), account.interests, repository, eventBus)
+            }
+        }
 
-        ()
+        val accounts = repository.listAccountsAndTheirInterestRates
+        val entries = buildEntriesForInterestBearingAccounts(accounts)
+        repository.saveEntries(entries)
+        publishInterestEntriesCreated(entries)
     }
 
     override def receive(event: Event): Unit = event match {
